a3cbe41ef84cf9fedef3ddcaf386eae61bd50d2a
Adidem23 <adityasuryawanshi5451@gmail.com>
2024-05-23 12:07:58 +0530

    Feat:Updated PhpLoginMaxFiles

--- PhpLogin/login.php
+++ PhpLogin/login.php
@@ -1,36 +1,17 @@
 <?php
 
-// Maximum allowed concurrent connections
-$max_connections = 3;
+    if(isset($_POST['login'])){
+        $username = $_POST['username'];
+        $password = $_POST['password'];
 
-// File used for storing connection count
-$lock_file = 'connection_count.txt';
-
-// Acquire lock (exclusive access to file)
-$fp = fopen($lock_file, 'a+');
-if (!$fp || !flock($fp, LOCK_EX | LOCK_NB)) {
-    echo 'denied';
-    fclose($fp);
-    exit;
-}
-
-// Read current connection count (or create file if it doesn't exist)
-$count = (int)fgets($fp);
-
-// Check if limit is reached
-if ($count >= $max_connections) {
-    echo 'denied';
-} else {
-    // Increment connection count
-    fseek($fp, 0, SEEK_SET);
-    ftruncate($fp, 0);
-    fwrite($fp, strval($count + 1));
-
-    echo $count+1;
-}
-
-// Release lock
-flock($fp, LOCK_UN);
-fclose($fp);
+        if($username == "admin" and $password == "admin"){
+            session_start();
+            $_SESSION['username'] = $username;
+            header("Location: home.php");
+        }
+        else{
+            echo "Wrong username and password";
+        }
+    }
 
 ?>
--- PhpLogin/logout.php
+++ PhpLogin/logout.php
@@ -1,29 +1,10 @@
 <?php
+session_start();
 
-// File used for storing connection count
-$lock_file = 'connection_count.txt';
+// Destroy the session
+session_destroy();
 
-// Acquire lock (exclusive access to file)
-$fp = fopen($lock_file, 'a+');
-if (!$fp || !flock($fp, LOCK_EX | LOCK_NB)) {
-    echo 'error'; // Indicate an error if lock cannot be acquired
-    exit;
-}
-
-// Read current connection count
-$count = (int)fgets($fp);
-
-// Decrement connection count (assuming successful logout)
-if ($count > 0) {
-    fseek($fp, 0, SEEK_SET);
-    ftruncate($fp, 0);
-    fwrite($fp, strval($count - 1));
-
-    echo $count-1;
-}
-
-// Release lock
-flock($fp, LOCK_UN);
-fclose($fp);
-
+// Redirect to the login page or any other page after logout
+header("Location: index.html");
+exit();
 ?>
