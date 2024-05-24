f5f06662a3f52dc29ae2ade6813d3075197d5507
Aditya Suryawanshi <124609794+Adidem23@users.noreply.github.com>
2024-05-24 07:03:37 +0530

    Update process.php

--- UPPERCASE_TO_LOWERCASE_PHP/process.php
+++ UPPERCASE_TO_LOWERCASE_PHP/process.php
@@ -1,16 +1,34 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get the input text
-    $inputText = $_POST['inputText'];
+<!DOCTYPE html>
+<html>
 
-    // Convert the text to lowercase
-    $lowercaseText = strtolower($inputText);
+<head>
+    <link rel="stylesheet" href="Stylesheet.css">
+</head>
 
-    // Make the first character of each word uppercase
-    $transformedText = ucwords($lowercaseText);
+<body>
 
-    // Display the transformed text
-    echo "<h1>Transformed Text</h1>";
-    echo "<p>$transformedText</p>";
-}
-?>
+    <form method="post">
+        <label for="inputString">Enter a string:</label><br>
+        <input type="text" id="inputString" name="inputString"><br>
+        <input type="submit" name="toUpperCase" value="Transform to Uppercase">
+        <input type="submit" name="toLowerCase" value="Transform to Lowercase">
+        <input type="submit" name="capitalizeFirst" value="Capitalize First Character">
+    </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $inputString = $_POST['inputString'];
+
+        if (isset($_POST['toUpperCase'])) {
+            echo "<center>" . strtoupper($inputString) . "</center>";
+        } elseif (isset($_POST['toLowerCase'])) {
+            echo "<center>" . strtolower($inputString) . "</center>";
+        } elseif (isset($_POST['capitalizeFirst'])) {
+            echo "<center>" . ucfirst($inputString) . "</center>";
+        }
+    }
+    ?>
+
+</body>
+
+</html>
